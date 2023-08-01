affe6c5b16c6123070d535774b27a065b23d6221
HenryCode4 <henrycnnamani4@gmail.com>
2023-08-01 13:25:48 -0700

    added verify email

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,36 +1,36 @@
-'use client';
-import Link from "next/link"
-import React, { useEffect, useState } from 'react'
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
 import {useRouter} from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
 
 
-export default function SignupPage(){
-    const router = useRouter();
 
-    const [user, setUser] = useState({
+export default function SignupPage() {
+    const router = useRouter();
+    const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: "",
     })
-
-    const [buttonDisabled, setButtonDisabled] = useState(false)
-    const [loading, setLoading] = useState(false);
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
     const onSignup = async () => {
         try {
             setLoading(true);
-            const response = await axios.post("/api/users/signup", user)
+            const response = await axios.post("/api/users/signup", user);
             console.log("Signup success", response.data);
             router.push("/login");
-    
+            
         } catch (error) {
-            console.log("signup failed", error.message)
-            toast.error(error.message)
-        }finally{
-            setLoading(false)
+            console.log("Signup failed", error.message);
+            
+            toast.error(error.message);
+        }finally {
+            setLoading(false);
         }
     }
 
@@ -42,44 +42,43 @@ export default function SignupPage(){
         }
     }, [user]);
 
-    
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="p-12 font-bold text-4xl">Signup</h1>
-            <hr />
-            <label htmlFor="username">username</label>
-            <input 
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>{loading ? "Processing" : "Signup"}</h1>
+        <hr />
+        <label htmlFor="username">username</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="username"
             type="text"
             value={user.username}
-            onChange={(e)=> setUser({...user, username: e.target.value})} 
-            placeholder="Username"/>
-
-
-            <label htmlFor="email">email</label>
-            <input 
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            onChange={(e) => setUser({...user, username: e.target.value})}
+            placeholder="username"
+            />
+        <label htmlFor="email">email</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="email"
             type="text"
             value={user.email}
-            onChange={(e)=> setUser({...user, email: e.target.value})} 
-            placeholder="Email"/>
-
-
-            <label htmlFor="password">Password</label>
-            <input 
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+        <label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="password"
             type="password"
             value={user.password}
-            onChange={(e)=> setUser({...user, password: e.target.value})} 
-            placeholder="Password"/>
-
-            <button 
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+            <button
             onClick={onSignup}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{loading ? "Processing" : "Signup"}</button>
-            <Link href="/login">Visit Login Page</Link>
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
+            <Link href="/login">Visit login page</Link>
         </div>
     )
+
 }
